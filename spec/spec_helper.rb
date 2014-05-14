b16abb64bf4e4ebf4831bfba3107b56f1864156b
Mark Wise <markmediadude@gmail.com>
2014-05-14 01:35:52 -0500

    only run coverage report if passing COVERAGE=true to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 
-require 'simplecov'
-SimpleCov.start
+if ENV["COVERAGE"]
+  require 'simplecov'
+  SimpleCov.start
+end
 
 require 'rb-music'
